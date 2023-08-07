7633abf50c8f2d26138300c78e37ed2770be44c7
Le Quy Long <longkold00@gmail.com>
2023-08-07 10:32:32 +0700

    + add complete tube effect

--- src/iec/scenes/playScene.js
+++ src/iec/scenes/playScene.js
@@ -4,6 +4,9 @@ import { Scene } from "../../pureDynamic/PixiWrapper/scene/scene";
 import { GameResizer } from "../../pureDynamic/systems/gameResizer";
 import { Level } from "../level/level";
 import { BackgroundManager } from "../../iec/object/background/backgroundManager";
+import { Confetti } from "../../iec/object/confetti/confetti";
+import { Spawner } from "../../spawners/spawner";
+import { SoundManager } from "../../soundManager";
 
 export class PlayScene extends Scene {
   constructor() {
@@ -29,14 +32,36 @@ export class PlayScene extends Scene {
     this.addChild(this.bgManager);
   }
 
-  _initLevelEasy() {
+  _initLevel() {
     this.level = new Level(0);
+    this._registerLevelEventListener();
     this.gameplay.addChild(this.level);
   }
 
   _initGameplay() {
     this.gameplay = new Container();
     this.addChild(this.gameplay);
-    this._initLevelEasy();
+    this._initLevel();
+    this._initConfetti();
+  }
+
+  _registerLevelEventListener() {
+    this.level.on("spawnConfetti", this._spawnConfetti, this);
+  }
+
+  _initConfetti() {
+    this.confettiSpawner = new Spawner();
+    this.confettiSpawner.init(() => {
+      return new Confetti();
+    }, 2);
+  }
+
+  _spawnConfetti(tube) {
+    SoundManager.play("sfx_cheer");
+    let confetti = this.confettiSpawner.spawn();
+    this.gameplay.addChild(confetti);
+    confetti.x = tube.x;
+    confetti.y = tube.y;
+    confetti.play();
   }
 }
