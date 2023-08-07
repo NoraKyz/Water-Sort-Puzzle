c17bceb1305504d191deba673692c6dbdc3c864e
Le Quy Long <longkold00@gmail.com>
2023-08-07 16:10:55 +0700

    + create topbarUI

---
+++ src/iec/ui/topbarUI/titleLevel.js
@@ -0,0 +1,19 @@
+import { Text } from "pixi.js";
+
+export class TitleLevel extends Text {
+  constructor(idLevel) {
+    super(`Level: ${idLevel}`, {
+      fill: "#ffebef",
+      fontFamily: "Comic Sans MS",
+      fontSize: 64,
+      fontWeight: "bolder"
+    });
+
+    this.anchor.set(0.5);
+    this.position.set(100, 50);
+  }
+
+  _setText(idLevel) {
+    this.text = `Level: ${idLevel}`;
+  }
+}
---
+++ src/iec/ui/topbarUI/topbarUI.js
@@ -0,0 +1,19 @@
+import { Container } from "pixi.js";
+
+export class TopbarUI extends Container {
+    constructor() {
+        super();
+        this._initProperties();
+        this._initComponents();
+    }
+
+    _initProperties() {
+    }
+
+    _initComponents() {
+    }
+
+    _initTitleLevel() {
+        
+    }
+}
