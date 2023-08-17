e30dc953997f4b9eee3e6bf9c3483865f3b64a11
Le Quy Long <longkold00@gmail.com>
2023-08-17 16:04:05 +0700

    test

--- src/iec/object/skin/skinManager.js
+++ src/iec/object/skin/skinManager.js
@@ -1,13 +1,37 @@
 import TubeData from "../../../../assets/jsons/tubeData.json";
+import ThemeData from "../../../../assets/jsons/backgroundData.json";
+import { Data } from "../../../dataTest";
 
 export class SkinManager {
-    static _currentSkin = TubeData[0];
+    static _currTubeSkin = TubeData[Data.tubeSkinId-1];
+    static _currThemeSkin = ThemeData[Data.themeSkinId-1];
+    
 
-    static get currentSkin() {
-        return this._currentSkin;
+    static get Tube() {
+        return this._currTubeSkin;
     }
 
-    static setSkin(skinData) {
-        this._currentSkin = value;
+    static get Theme() {
+        return this._currThemeSkin;
+    }
+
+    static set Tube(id) {
+        this._currTubeSkin.state = "unlocked";
+        this._currTubeSkin = ThemeData[id-1];
+        this._currTubeSkin.state = "selected";
+    }
+
+    static set Theme(id) {
+        this._currThemeSkin.state = "unlocked";
+        this._currThemeSkin = TubeData[id-1];
+        this._currThemeSkin.state = "selected";     
+    }
+
+    static get TubeData() {
+        return TubeData;
+    }
+
+    static get ThemeData() {
+        return ThemeData;
     }
 }
