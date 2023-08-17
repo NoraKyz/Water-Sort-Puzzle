8c2aafe0a79a268d57b2f907bd70efc00fbe7311
Le Quy Long <longkold00@gmail.com>
2023-08-17 11:00:20 +0700

    + add data observe

--- src/dataTest.js
+++ src/dataTest.js
@@ -1,8 +1,41 @@
+export const DataType = Object.freeze({
+    Coin: "coin",
+});
 
 export class Data {
     static init() {
+        this.observes = [];
         this.addTubeTimes = 2;
         this.undoTimes = 5;
         this.currentLevel = 0;
+        this.coin = 10000;
+    }
+
+    static addObserver(observer) {
+        this.observes.push(observer);
+    }
+
+    static removeObserver(observer) {
+        let index = this.observes.indexOf(observer);
+        if (index > -1) {
+            this.observe.splice(index, 1);
+        }
+    }
+
+    static _notify() {
+        this.observes.forEach(observe => {
+            observe.emit("dataChange");
+        });
+    }
+
+    static change(type, value) {
+        switch (type) {
+            case DataType.Coin: {
+                this.coin += value;
+                break;
+            }   
+        }
+
+        this._notify();
     }
 }
