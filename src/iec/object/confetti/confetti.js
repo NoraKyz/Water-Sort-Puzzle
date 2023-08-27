36daa4406b7c5d0d3d991c59eb52a83b9896029b
Le Quy Long <longkold00@gmail.com>
2023-08-27 22:58:56 +0700

    - change pos confetti

--- src/iec/object/confetti/confetti.js
+++ src/iec/object/confetti/confetti.js
@@ -8,7 +8,7 @@ export class Confetti extends Container {
     let textures = Util.getTexturesContain("image_014_");
     this.animtedSprite = new AnimatedSprite(textures, true);
     this.animtedSprite.anchor.set(0.5);
-    this.animtedSprite.y = -250;
+    this.animtedSprite.y = -290;
     this.animtedSprite.animationSpeed = 0.8;
     this.animtedSprite.loop = false;
     this.animtedSprite.onComplete = () => {
